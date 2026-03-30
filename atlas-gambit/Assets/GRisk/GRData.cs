d2d024f95d5e7cf0b1231a49001e2566bbb5cf8f
kupofti <williambriand07@gmail.com>
2026-03-29 21:55:41 -0400

    basic GRisk components (stash)

---
+++ atlas-gambit/Assets/GRisk/GRData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GRData
+{
+    public static TerritoryDataList territoryData = JLoader<TerritoryDataList>.load("territories");
+}
---
+++ atlas-gambit/Assets/GRisk/TerritoryData.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TerritoryData
+{
+    public string id;
+    public string[] adjacencies;
+}
+
+[Serializable]
+public class TerritoryDataList
+{
+    public TerritoryData[] territories;
+
+    public Dictionary<string, TerritoryData> asDict()
+    {
+        Dictionary<string, TerritoryData> terrDict = new Dictionary<string, TerritoryData>();
+        foreach (TerritoryData territory in territories)
+        {
+            terrDict[territory.id] = territory;
+        }
+
+        return terrDict;
+    }
+}
