2fce82551a6ef542b2b13324ae01722c117dd182
kupofti <williambriand07@gmail.com>
2026-04-11 16:15:23 -0400

    small formatting change

--- atlas-gambit/Assets/GRisk/util/JLoader.cs
+++ atlas-gambit/Assets/GRisk/util/JLoader.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JLoader<T> where T : class
+public class JLoader<T>
+    where T : class
 {
-	public static T load(string resourcePath)
-	{
-		TextAsset jsonFile = Resources.Load<TextAsset>(resourcePath);
+    public static T load(string resourcePath)
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>(resourcePath);
 
-		if (jsonFile == null)
-		{
-			Debug.LogError($"JSON file not found at {resourcePath}");
-			return null;
-		}
+        if (jsonFile == null)
+        {
+            Debug.LogError($"JSON file not found at {resourcePath}");
+            return null;
+        }
 
-		T data = JsonUtility.FromJson<T>(jsonFile.text);
+        T data = JsonUtility.FromJson<T>(jsonFile.text);
 
-		return data;
-	}
+        return data;
+    }
 }
