3725943b58ea797f9ca3f4ec86f423d729cd9255
kupofti <williambriand07@gmail.com>
2026-04-13 16:45:45 -0400

    start testing GREngine

---
+++ atlas-gambit/Assets/Scripts/GRiskTester.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using GRisk.Engine;
+
+public class GRiskTester : MonoBehaviour
+{
+    void Start()
+    {
+        var gr = new GR();
+        
+        Debug.Log(gr);
+    }
+}
