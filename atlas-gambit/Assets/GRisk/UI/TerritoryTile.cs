876992dd176b75ab200e432fa536cbdea4a8d0f4
kupofti <williambriand07@gmail.com>
2026-04-14 18:04:06 -0400

    Start of physical interface

---
+++ atlas-gambit/Assets/GRisk/UI/TerritoryTile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace GRisk.UI
+{
+    public class TerritoryTile : MonoBehaviour
+    {
+        public string territoryId;
+        
+        public UnityEvent propPlaced;
+        
+        private uint manpower;
+        private uint owner;
+
+        public void setState(uint[] state)
+        {
+            manpower = state[0];
+            owner = state[1];
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/UI/TileManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GRisk.UI
+{
+    public class TileManager : MonoBehaviour
+    {
+        
+    }
+}
