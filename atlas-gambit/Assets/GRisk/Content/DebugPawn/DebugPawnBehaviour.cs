bb83afdf17be130fd868225bccfb828eba2418cb
kupofti <williambriand07@gmail.com>
2026-05-05 13:33:57 -0400

    DebugPawn for land claiming demo

---
+++ atlas-gambit/Assets/GRisk/Content/DebugPawn/DebugPawnBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using GRisk.Data;
+using GRisk.Engine;
+using GRisk.Interaction.Item;
+using UnityEngine;
+
+namespace GRisk.Content.DebugPawn
+{
+    [RequireComponent(typeof(Renderer))]
+    public class DebugPawnBehaviour : ItemBehaviour
+    {
+        public DebugPawnConsumable consumable;
+
+        private new Renderer renderer;
+
+        private void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+            
+            updateColor();
+        }
+
+        public override void onSecondary()
+        {
+            consumable.owner = consumable.owner == GRTypes.Player.PLAYER ? GRTypes.Player.ENTITY : GRTypes.Player.PLAYER;
+            
+            updateColor();
+        }
+
+        private void updateColor()
+        {
+            renderer.material.color = GRData.playerStyleDict[(uint)consumable.owner].color;
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Content/DebugPawn/DebugPawnConsumable.cs
@@ -0,0 +1,17 @@
+﻿using GRisk.Data;
+using GRisk.Engine;
+using GRisk.Interaction.Item;
+
+namespace GRisk.Content.DebugPawn
+{
+    public class DebugPawnConsumable : ConsumableItem
+    {
+        public override void territoryEffect(string territoryId, GR engine)
+        {
+            if ((uint)GRTypes.Player.NONE == engine.ownerAt(territoryId))
+            {
+                engine.setOwnerAt(territoryId, (uint)owner);
+            }
+        }
+    }
+}
