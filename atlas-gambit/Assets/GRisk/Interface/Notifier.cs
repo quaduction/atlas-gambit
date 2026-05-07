29ab08ee246edcd6826937ed2883a9aa7b29b564
kupofti <williambriand07@gmail.com>
2026-05-07 18:18:19 -0400

    Notifier

---
+++ atlas-gambit/Assets/GRisk/Interface/Notifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using GRisk.Engine;
+using GRisk.Util;
+using TMPro;
+using UnityEngine;
+
+namespace GRisk.Interface
+{
+    public class Notifier : MonoBehaviour
+    {
+        public GRFacade facade;
+        public TMP_Text board;
+        
+        private FixedSizedQueue<string> logs = new();
+
+        private void Awake()
+        {
+            logs.Limit = 4;
+        }
+
+        private void Start()
+        {
+            update();
+        }
+
+        private void update()
+        {
+            board.SetText($"Player: {facade.engine.currentPlayer()}\n" +
+                          $"Phase: {facade.engine.currentPhase().ToString()}\n\n\n" +
+                          $"> {string.Join("\n >", logs)}");
+        }
+        
+        public void log(string msg)
+        {
+            Debug.Log(msg);
+            
+            logs.Enqueue(msg);
+            
+            update();
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Util/FixedSizeQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GRisk.Util
+{
+    public class FixedSizedQueue<T> : Queue<T>
+    {
+        public int Limit { get; set; }
+
+        public new void Enqueue(T obj)
+        {
+            base.Enqueue(obj);
+            
+            while (Count > Limit)
+            {
+                Dequeue();
+            }
+        }
+    }
+
+}
