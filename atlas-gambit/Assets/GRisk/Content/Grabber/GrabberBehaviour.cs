43dcbd759e7303b9b289fc50b8be9b6c4a98a471
kupofti <williambriand07@gmail.com>
2026-05-07 14:07:22 -0400

    Grabber focus test (incomplete)

---
+++ atlas-gambit/Assets/GRisk/Content/Grabber/GrabberBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using GRisk.Interaction.Item;
+
+namespace GRisk.Content.Grabber
+{
+    public class GrabberBehaviour : ItemBehaviour
+    {
+        public ConsumableItem head;
+
+        public override void offTrigger()
+        {
+            head.releaseFocus();
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Content/Grabber/GrabberConsumable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GRisk.Interaction.Item;
+
+namespace GRisk.Content.Grabber
+{
+    public class GrabberConsumable : ConsumableItem
+    {
+        private void Reset()
+        {
+            destroyOnConsume = false;
+            applyFocus = true;
+            focusLock = true;
+        }
+    }
+}
