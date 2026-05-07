6c1bb8a73ff7e18e736072fe0331e6b07b87838c
kupofti <williambriand07@gmail.com>
2026-05-07 18:20:46 -0400

    Grabber works

--- atlas-gambit/Assets/GRisk/Content/Grabber/GrabberBehaviour.cs
+++ atlas-gambit/Assets/GRisk/Content/Grabber/GrabberBehaviour.cs
@@ -1,14 +1,48 @@
 ﻿using GRisk.Interaction.Item;
+using GRisk.Interface;
+using TMPro;
+using UnityEngine;
 
 namespace GRisk.Content.Grabber
 {
     public class GrabberBehaviour : ItemBehaviour
     {
-        public ConsumableItem head;
+        public GrabberConsumable head;
+        public TMP_Text moveAmountLabel;
+
+        public uint moveAmount = 0u;
+        public uint moveIncrement = 10u;
+
+
+        public override void onTrigger()
+        {
+            head.applyFocus = true;
+        }
 
         public override void offTrigger()
         {
+            head.applyFocus = false;
             head.releaseFocus();
+        }
+
+        public override void onPrimary()
+        {
+            moveAmount += moveIncrement;
+            updateMoveAmount();
+        }
+
+        public override void onSecondary()
+        {
+            if (moveAmount < moveIncrement) return;
+
+            moveAmount -= moveIncrement;
+            updateMoveAmount();
+        }
+
+        private void updateMoveAmount()
+        {
+            head.moveAmount = moveAmount;
+            moveAmountLabel.SetText(NumberLabel.formatCompact(moveAmount));
         }
     }
 }
--- atlas-gambit/Assets/GRisk/Content/Grabber/GrabberConsumable.cs
+++ atlas-gambit/Assets/GRisk/Content/Grabber/GrabberConsumable.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using GRisk.Engine;
 using GRisk.Interaction.Item;
 
 namespace GRisk.Content.Grabber
 {
     public class GrabberConsumable : ConsumableItem
     {
+        public uint moveAmount = 0u;
+        
         private void Reset()
         {
             destroyOnConsume = false;
-            applyFocus = true;
             focusLock = true;
+        }
+
+        public override void secondaryFocusEffect(GRFacade facade, string territoryId)
+        {
+            if (focusedTile == null) return;
+            
+            facade.move(focusedTile.territoryId, territoryId, moveAmount);
         }
     }
 }
