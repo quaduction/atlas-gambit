a0f55c2c2653f2eab142d23e214500e0df612a40
LishunSLD <seanlarrydriesen@gmail.com>
2026-05-08 12:37:02 -0400

    Grenade and Fake Grenade

---
+++ atlas-gambit/Assets/GRisk/Content/Grenade/GrenadeBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using GRisk.Data;
+using GRisk.Engine;
+using GRisk.Interaction.Item;
+using UnityEngine;
+
+namespace GRisk.Content.Grenade
+{
+    [RequireComponent(typeof(Renderer))]
+    public class GrenadeBehaviour : ItemBehaviour
+    {
+        public GrenadeConsumable consumable;
+
+        private new Renderer renderer;
+
+        private void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Content/Grenade/GrenadeConsumable.cs
@@ -0,0 +1,16 @@
+﻿using GRisk.Engine;
+using GRisk.Interaction.Item;
+
+namespace GRisk.Content.Grenade
+{
+    public class GrenadeConsumable : ConsumableItem
+    {
+        public override void territoryEffect(GRFacade facade, string territoryId)
+        {
+            if ((uint)GRTypes.Player.NONE == facade.engine.ownerAt(territoryId))
+            {
+                facade.engine.setOwnerAt(territoryId, (uint)owner);
+            }
+        }
+    }
+}
