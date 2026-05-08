c1e93f93d92d154e9ada9da2b4b71ba2547bb432
LishunSLD <seanlarrydriesen@gmail.com>
2026-05-08 12:45:52 -0400

    Revolver Bullet

---
+++ atlas-gambit/Assets/GRisk/Content/RevolverBullet/RevolverBulletBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using GRisk.Data;
+using GRisk.Engine;
+using GRisk.Interaction.Item;
+using UnityEngine;
+
+namespace GRisk.Content.Bullet
+{
+    [RequireComponent(typeof(Renderer))]
+    public class BulletBehaviour : ItemBehaviour
+    {
+        public BulletConsumable consumable;
+
+        private new Renderer renderer;
+
+        private void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Content/RevolverBullet/RevolverBulletConsumable.cs
@@ -0,0 +1,16 @@
+﻿using GRisk.Engine;
+using GRisk.Interaction.Item;
+
+namespace GRisk.Content.Bullet
+{
+    public class BulletConsumable : ConsumableItem
+    {
+        public override void territoryEffect(GRFacade facade, string territoryId)
+        {
+            if ((uint)GRTypes.Player.NONE == facade.engine.ownerAt(territoryId))
+            {
+                facade.engine.setOwnerAt(territoryId, (uint)owner);
+            }
+        }
+    }
+}
