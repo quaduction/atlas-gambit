9bd31b02ef91300aed7a1818919a84a1658127c8
LishunSLD <seanlarrydriesen@gmail.com>
2026-05-11 11:50:56 -0400

    Base pawns

---
+++ atlas-gambit/Assets/GRisk/Content/Pawn/PawnBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using GRisk.Data;
+using GRisk.Engine;
+using GRisk.Interaction.Item;
+using UnityEngine;
+
+namespace GRisk.Content.Pawn
+{
+    [RequireComponent(typeof(Renderer))]
+    public class PawnBehaviour : ItemBehaviour
+    {
+        public PawnConsumable consumable;
+
+        private new Renderer renderer;
+
+        private void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+            
+            updateColor();
+        }
+
+        public override void onSecondary()
+        {
+            consumable.owner = consumable.owner == GRTypes.Player.PLAYER ? GRTypes.Player.ENTITY : GRTypes.Player.PLAYER;
+            
+            updateColor();
+        }
+
+        private void updateColor()
+        {
+            renderer.material.color = GRData.playerStyleDict[(uint)consumable.owner].color;
+        }
+    }
+}
---
+++ atlas-gambit/Assets/GRisk/Content/Pawn/PawnConsumable.cs
@@ -0,0 +1,16 @@
+﻿using GRisk.Engine;
+using GRisk.Interaction.Item;
+
+namespace GRisk.Content.Pawn
+{
+    public class PawnConsumable : ConsumableItem
+    {
+        public override void territoryEffect(GRFacade facade, string territoryId)
+        {
+            if ((uint)GRTypes.Player.NONE == facade.engine.ownerAt(territoryId))
+            {
+                facade.engine.setOwnerAt(territoryId, (uint)owner);
+            }
+        }
+    }
+}
